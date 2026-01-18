9210cf172d9907b56787832982622083d7a752bf
Parth Jhalani <163110816+parthjhalani11@users.noreply.github.com>
2026-01-18 16:34:27 +0530

    Create numbers_practice.py

---
+++ day1_numbers/numbers_practice.py
@@ -0,0 +1,42 @@
+                                                               #Integers and Floats - Working with Numeric Data
+                                                               
+
+#Q1
+'''a = 10
+b = 3
+
+print(a/b)
+print(type(a/b))'''
+
+#Q2
+'''a = 10
+b = 3
+
+print("Integer division result :",a//b)
+print("remainder :", a%b)'''
+
+#Q3
+'''price = 19.99
+quantity = 3
+
+print("Total :",round(price*quantity,2))'''
+
+#Q4
+'''data = "450"
+tax = 0.18
+
+fdata = float(data)
+tax_amount = fdata*tax
+print("Total amount with tax :",fdata + tax_amount)'''
+
+#Q5
+'''amounts = [100, 250.50, 349, 99.99]
+
+total_amount = sum(amounts)
+print(total_amount)
+mean = round(total_amount/len(amounts),2)
+print(mean)'''
+
+#Q6
+value = 2.675
+print(round(value, 2))
